db32b1d640b401945815d7d7e84b6ccfe15e3d9c
GabrielPinto <camilo1234299@gmail.com>
2025-04-28 17:42:12 -0500

    Creando los repositories

---
+++ src/main/java/com/example/demo/repository/ResultadoRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entidades.Resultado;
+
+public interface ResultadoRepository extends JpaRepository<Resultado, Integer>{
+
+}
